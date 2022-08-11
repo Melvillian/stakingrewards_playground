ce95c27fc95ed6a6c1d8f274e1b9b965d0e775c9
Melvillian <melvillevt@gmail.com>
2022-08-11 13:56:23 -0400

    extend the scenario by having bob stake 100 stakingToken at 6/8ths through and unstake at 7/ths through. This shows how the rewardPerToken always grows, and how it is used to correctly keep track of other stakers' rewards even though they do not interact with the protocol (i.e. call updateReward)

--- test/StakingRewards.ts
+++ test/StakingRewards.ts
@@ -9,32 +9,15 @@ import { StakingRewards } from "../typechain-types";
 let stakingRewards: StakingRewards
 let owner: SignerWithAddress
 let alice: SignerWithAddress
+let bob: SignerWithAddress
 
 describe("StakingRewards", function () {
-  // We define a fixture to reuse the same setup in every test.
-  // We use loadFixture to run this setup once, snapshot that state,
-  // and reset Hardhat Network to that snapshopt in every test.
-  async function deployOneYearLockFixture() {
-    const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-    const ONE_GWEI = 1_000_000_000;
-
-    const lockedAmount = ONE_GWEI;
-    const unlockTime = (await time.latest()) + ONE_YEAR_IN_SECS;
-
-    // Contracts are deployed using the first signer/account by default
-    const [owner, otherAccount] = await ethers.getSigners();
-
-    const Lock = await ethers.getContractFactory("Lock");
-    const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-    return { lock, unlockTime, lockedAmount, owner, otherAccount };
-  }
 
   const HUNDRED_ETH = ethers.utils.parseEther("100")
   const ONE_ETH = ethers.utils.parseEther("1")
 
   it("should manage StakingRewards lifecycle", async () => {
-    [owner, alice] = await ethers.getSigners();
+    [owner, alice, bob] = await ethers.getSigners();
 
     // deploy contracts
     const SimpleERC20 = await ethers.getContractFactory("SimpleERC20");
@@ -47,6 +30,7 @@ describe("StakingRewards", function () {
     // setup ERC20
     await erc20Staking.mint(owner.address, HUNDRED_ETH)
     await erc20Staking.mint(alice.address, HUNDRED_ETH)
+    await erc20Staking.mint(bob.address, HUNDRED_ETH)
 
     await erc20Rewards.mint(owner.address, HUNDRED_ETH)
 
@@ -64,10 +48,10 @@ describe("StakingRewards", function () {
     console.log("after stake")
     await log()
 
-    // fast forward halfway
+    // fast forward to 4/8th's through
     await time.increase(60 * 60 * 24 * 7 / 2)
 
-    console.log("after increasing time by half the rewardsDuration")
+    console.log("after increasing time by 4/8ths the rewardsDuration")
     await log()
 
     // have alice stake 100 LP Token
@@ -77,13 +61,34 @@ describe("StakingRewards", function () {
     console.log("after alice stakes")
     await log()
 
-    // increase time to 3/4 through the reward period
+    // increase time to 6/8 through the reward period
     await time.increase(60 * 60 * 24 * 7 / 4)
 
-    console.log("after 3/4ths done")
+    console.log("after 6/8ths done")
     await log()
 
-    // increase time to the end of the period
+    // add a third staker, bob, who stakes for 1/8 of the time and then exits
+    // this will help us see how intermediate changes in the rewardPerToken and
+    // rewardPerTokenStored affect the owner and alice
+    await erc20Staking.connect(bob).approve(stakingRewards.address, HUNDRED_ETH)
+    await stakingRewards.connect(bob).stake(HUNDRED_ETH)
+
+    console.log("after bob stake")
+    await log()
+
+    // increase time to 7/8ths through the reward period
+    await time.increase(60 * 60 * 24 * 7 / 8)
+
+    console.log("after 7/8ths done")
+    await log()
+
+    // have bob exit, so the total supply is brought back down to 200
+    await stakingRewards.connect(bob).exit()
+
+    console.log("after bob exit")
+    await log()
+
+    // increase time to the end (8/8ths) of the period
     const periodFinish = await stakingRewards.periodFinish()
     await time.increaseTo(periodFinish)
 
@@ -97,8 +102,9 @@ describe("StakingRewards", function () {
     console.log("after exit")
     await log()
 
-    console.log("owner balance WETH", await (await erc20Rewards.balanceOf(owner.address)).div(ONE_ETH))
-    console.log("alice balance WETH", await (await erc20Rewards.balanceOf(alice.address)).div(ONE_ETH))
+    console.log("owner balance WETH", Number(await (await erc20Rewards.balanceOf(owner.address)).toString()) / 1e18)
+    console.log("alice balance WETH", Number(await (await erc20Rewards.balanceOf(alice.address)).toString()) / 1e18)
+    console.log("bob balance WETH", Number(await (await erc20Rewards.balanceOf(bob.address)).toString()) / 1e18)
 
   })
 
@@ -109,10 +115,12 @@ describe("StakingRewards", function () {
     console.log("rewardRate                   ", await (await stakingRewards.rewardRate()).toString())
     console.log("rewardPerToken               ", Number(await (await stakingRewards.rewardPerToken()).toString()) / 1e18)
     console.log("totalSupply                  ", await (await stakingRewards.totalSupply()).div(ONE_ETH).toString())
-    console.log("owner earned                 ", await (await stakingRewards.earned(owner.address)).div(ONE_ETH).toString())
-    console.log("alice earned                 ", await (await stakingRewards.earned(alice.address)).div(ONE_ETH).toString())
+    console.log("owner earned                 ", Number(await (await stakingRewards.earned(owner.address)).toString()) / 1e18)
+    console.log("alice earned                 ", Number(await (await stakingRewards.earned(alice.address)).toString()) / 1e18)
+    console.log("bob earned                   ", Number(await (await stakingRewards.earned(bob.address)).toString()) / 1e18)
     console.log("owner userRewardPerTokenPaid ", Number(await (await stakingRewards.userRewardPerTokenPaid(owner.address)).toString()) / 1e18)
     console.log("alice userRewardPerTokenPaid ", Number(await (await stakingRewards.userRewardPerTokenPaid(alice.address)).toString()) / 1e18)
+    console.log("bob userRewardPerTokenPaid   ", Number(await (await stakingRewards.userRewardPerTokenPaid(bob.address)).toString()) / 1e18)
 
     console.log("\n")
   }
